a0b5d5dd56eb50b2ebe432be8c6e21b84a20f08b
Devendra Singh Shekhawat <devendra.shekhawat@thepsi.com>
2020-12-25 01:57:58 +0530

    Final commit

--- src/authProvider.js
+++ src/authProvider.js
@@ -5,7 +5,9 @@ const config = {
   auth: {
     authority: 'https://login.microsoftonline.com/12077ea8-3f59-4127-a02a-2b2dc0e36027',
     clientId: azureId,
-    redirectUri: "http://localhost:3000/private"
+    redirectUri: "http://localhost:3000/private",
+    navigateToLoginRequestUrl: false
+
   },
   cache: {
     cacheLocation: "localStorage",
@@ -24,7 +26,8 @@ const authenticationParameters = {
 // Options
 const options = {
   loginType: LoginType.Redirect,
-  tokenRefreshUri: "http://localhost:3000/private"
+  tokenRefreshUri: "http://localhost:3000/private",
+
 }
 
 export const authProvider = new MsalAuthProvider(config, authenticationParameters, options)
